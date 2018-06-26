ca8409c140fc51372d655fc6b02dc2676b3329b3
Lewis Moore <lewis.moore2707@gmail.com>
2018-06-26 09:19:54 +0100

    Update aws region and AMI

--- example.tf
+++ example.tf
@@ -1,10 +1,10 @@
 provider "aws" {
   access_key = "${var.access_key}"
   secret_key = "${var.secret_key}"
-  region     = "us-east-1"
+  region     = "eu-west-1"
 }
 
 resource "aws_instance" "example" {
-  ami           = "ami-2757f631"
+  ami           = "ami-7c491f05"
   instance_type = "t2.micro"
 }
